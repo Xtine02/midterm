90785944865e5d32352871c22003f12458d6b8b0
unknown <YY201@ccs.local>
2025-03-10 05:48:25 -0700

    question3 updated

--- question2.js
+++ question2.js
@@ -1,7 +1,7 @@
 function filterEvenNumbers(numbers) {
     return numbers.filter(number => number % 2 === 0);
-  }
-
+}
+// modawat ug array then i filter kung even ba
 const numbers = [1,2,3,4,5,6];  
-console.log(filterEvenNumbers(numbers)); 
+console.log(filterEvenNumbers(numbers)); //tawagon ang function nga naka filters even number the output
 
